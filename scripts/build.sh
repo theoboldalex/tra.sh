7596d41df4d9d4b7043f140c5facfaa2b9ceec1a
theoboldalex <theoboldalex@gmail.com>
2023-08-03 21:28:31 +0100

    rejiggidy

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 find content -iname "*.md" -exec sh -c 'OUT=$(echo "{}" | sed "s/content\/[0-9]\{4\}\/[0-9]\{2\}\(.*\).md/public\1.htmlbody/"); markdown "{}" > "$OUT"' {} \;
-find public -iname "*.htmlbody" -exec sh -c 'NAME=$(echo "{}" | sed "s/htmlbody/html/"); cat header.html "{}" footer.html > "$NAME"' \;
+find public -iname "*.htmlbody" -exec sh -c 'NAME=$(echo "{}" | sed "s/htmlbody/html/"); cat partials/header.html "{}" partials/footer.html > "$NAME"' \;
 find public -iname "*.htmlbody" -exec sh -c 'rm "{}"' \;
 
 
