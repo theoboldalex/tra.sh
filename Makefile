a8330780edc3eb0df3d3e58e02e50de5390d4063
theoboldalex <theoboldalex@gmail.com>
2023-08-04 21:09:54 +0100

    suppress build output on make

--- Makefile
+++ Makefile
@@ -1,9 +1,3 @@
-links:
-	find content -iname '*.md' | \
-	sed -e 's/\(.*\)\.md/\<a href="\1.html">\1\<\/a\>/g' -e 's/content\/[0-9]\{4\}\/[0-9]\{2\}\///g' -e 's/\(.*\)\.md/\1/' > public/links.html
-	cat partials/header.html partials/hero.html public/links.html partials/footer.html > public/index.html
-	rm public/links.html
-
 build:
-	./scripts/build.sh
+	@./scripts/build.sh
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# generate hyperlinks for index page
+find content -iname '*.md' | \
+sed -e 's/\(.*\)\.md/\<a href="\1.html">\1\<\/a\>/g' -e 's/content\/[0-9]\{4\}\/[0-9]\{2\}\///g' -e 's/\(.*\)\.md/\1/' > public/links.html
+cat partials/header.html partials/hero.html public/links.html partials/footer.html > public/index.html
+rm public/links.html
+
+# convert content to html
 find content -iname "*.md" -exec sh -c 'OUT=$(echo "{}" | sed "s/content\/[0-9]\{4\}\/[0-9]\{2\}\(.*\).md/public\1/"); markdown "{}" > "$OUT"' {} \;
 find public -type f ! -name "*.*" -exec sh -c 'NAME=$(echo "{}" | sed "s/$/\.html/"); cat partials/header.html "{}" partials/footer.html > "$NAME"' \;
 find public -type f ! -name "*.*" -exec sh -c 'rm "{}"' \;
-
-
 
