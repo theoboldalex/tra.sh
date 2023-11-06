ae3f6faee6148ee4bc333f64f10677ac15789410
theoboldalex <theoboldalex@gmail.com>
2023-11-06 21:49:57 +0000

    adds line breaks on links

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 # generate hyperlinks for index page
 find content -iname '*.md' | \
-sed -e 's/\(.*\)\.md/\<a href="\1.html">\1\<\/a\>/g' -e 's/content\/[0-9]\{4\}\/[0-9]\{2\}\///g' -e 's/\(.*\)\.md/\1/' > public/links.html
+sed -e 's/\(.*\)\.md/\<a href="\1.html">\1\<\/a\>\<\br\>/g' -e 's/content\/[0-9]\{4\}\/[0-9]\{2\}\///g' -e 's/\(.*\)\.md/\1/' > public/links.html
 cat partials/header.html partials/hero.html public/links.html partials/footer.html > public/index.html
 rm public/links.html
 
