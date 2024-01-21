bc148be8ea962a91ade9957763279322fa5a3cc5
theoboldalex <theoboldalex@gmail.com>
2024-01-21 22:18:41 +0000

    comment

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 # generate hyperlinks for index page
 find content -iname '*.md' | \
 sed -e 's/\(.*\)\.md/\<a href="\1.html">\1\<\/a\>\<\br\>/g' -e 's/content\/[0-9]\{4\}\/[0-9]\{2\}\///g' -e 's/\(.*\)\.md/\1/' > public/links.html
+# create index page with posts listing
 cat partials/header.html partials/hero.html public/links.html partials/footer.html > public/index.html
 rm public/links.html
 
